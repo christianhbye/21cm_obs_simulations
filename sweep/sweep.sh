5f4352c97804580266d1d4fff67e71097f1d901d
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-04-13 22:33:33 -0400

    initial commit

---
+++ sweep/sweep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=00:12:00
+#SBATCH --array=7-60:1
+#SBATCH --mail-user=<christian.bye@mail.mcgill.ca>
+#SBATCH --mail-type=ALL
+
+source $HOME/conv_env/bin/activate
+export PYTHONPATH=$PYTHONPATH:/$SCRATCH/global21cm
+
+python run_parallel_conv.py $SLURM_ARRAY_TASK_ID
+
