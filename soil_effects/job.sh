268fc03a2a477a793e69ab579817c00630194187
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-06-16 16:51:46 -0400

    rename and change to sequential instead of parallel

--- soil_effects/job.sh
+++ soil_effects/job.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#SBATCH --time=3:30:00
-#SBATCH --array=1-3:1
+#SBATCH --time=6:30:00
 #SBATCH --nodes=1
 #SBATCH --mail-user=<christian.bye@mail.mcgill.ca>
 #SBATCH --mail-type=ALL
@@ -8,5 +7,7 @@
 source $HOME/conv_env/bin/activate
 export PYTHONPATH=$PYTHONPATH:/$SCRATCH/global21cm
 
-python run_many_parallel_conv.py $SLURM_ARRAY_TASK_ID
+python run_many_parallel_conv.py 1
+python run_many_parallel_conv.py 2
+python run_many_parallel_conv.py 3
 
